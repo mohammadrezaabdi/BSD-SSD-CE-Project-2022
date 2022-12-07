0a47acef80cc33edbbdb65b2dc5d37362b2f86d0
mohammadrezaabdi <wnick2085@gmail.com>
2022-12-07 13:49:28 +0000

    remove excess files

--- Apps/Main.c
+++ Apps/Main.c
@@ -21,14 +21,9 @@ command/data
 
 CHAR16 NVME_SERIAL_NUMBER[NVME_PRODUCT_SERIAL_NUMBER_SIZE] = {0};
 CHAR16 NVME_PRODUCT_MODEL_NUMBER[NVME_PRODUCT_MODEL_NUMBER_SIZE] = {0};
-CHAR16 USER_NVME_SERIAL_NUMBER[USER_NVME_PRODUCT_SERIAL_NUMBER_SIZE] = {0};
 
 
 UINT8 Verbose_Level = 1;
-UINT8 Dangerous_Zap = 1;
-UINT8 Dangerous_Zap_Dry_Run = 0;
-UINT8 Verbose_Dump = 0;
-UINT8 Save_GPTs = 1;
 
 
 VOID Usage(VOID)
@@ -48,8 +43,6 @@ VOID Usage(VOID)
 
 }
 
-#define NVME_ZAP_SHORTHAND_LENGTH 6
-
 INTN
 EFIAPI
 ShellAppMain (
@@ -64,65 +57,20 @@ ShellAppMain (
       Print (L"argv[%d]: %s\n", i, argv[i]);
     }
   }
-  //ShellSetPageBreakMode(TRUE);
   if (argc > 1) {
-    if (StriCmp(argv[1], L"ZAP") == 0) {
-      if (argc > 2) {
-        Dangerous_Zap = 1;
-        return NvmeZap (argv[2], MAX(StrLen(argv[2]), NVME_ZAP_SHORTHAND_LENGTH));
-      }
-      else {
-        Print (L"%s needs the NVME's serial number assiged.\n", argv[1]);
-        Usage();
-        return 1;
-      }
-    }
-    else if (StriCmp(argv[1], L"BACKUP") == 0 || StriCmp(argv[1], L"STORE") == 0) {
-      if (argc > 2) {
-        return NvmeGptSave (argv[2], MAX(StrLen(argv[2]), NVME_ZAP_SHORTHAND_LENGTH));
-      }
-      else {
-        Print (L"%s needs the NVME's serial number assiged.\n", argv[1]);
-        Usage();
-        return 1;
-      }
-    }
-    else if (StriCmp(argv[1], L"INFO") == 0) {
+    if (StriCmp(argv[1], L"INFO") == 0) {
       NvmeInfo();
+      //TODO: show ssd hash as key
     }
-    else if (StriCmp(argv[1], L"RESTORE") == 0) {
-      //NvmeRestore();
+    else if (StriCmp(argv[1], L"SAVE") == 0) {
+      //save ssd hash key to the database file
     }
-    else if (StriCmp(argv[1], L"DUMP") == 0) {
-      if (argc > 2) {
-        ShellSetPageBreakMode(TRUE);
-        //BUGBUG: page-break causes system hang when 'Q' is pressed.
-        return NvmeDump (argv[2], MAX(StrLen(argv[2]), NVME_ZAP_SHORTHAND_LENGTH), 0, 0);
-      }
-      else {
-        Print (L"%s needs the NVME's serial number assiged.\n", argv[1]);
-        Usage();
-        return 1;
-      }
+    else if (StriCmp(argv[1], L"CHECK") == 0) {
+      //check the ssd if it exists in database
     }
-//TODO    else if (StriCmp(argv[1], L"DEBUG") == 0) {
-//TODO      static UINTN HandleCount=0;
-//TODO      static EFI_HANDLE *HandleBuffer = NULL;
-//TODO      EFI_STATUS Status;
-//TODO      INT16 i;
-//TODO
-//TODO      ShellSetPageBreakMode(TRUE);
-//TODO      Status = gBS->LocateHandleBuffer(ByProtocol, &gEfiBlockIoProtocolGuid, NULL, &HandleCount, &HandleBuffer);
-//TODO      Print(L"Block IO, Instance Count: %d\n", HandleCount);
-//TODO      for (i=0; i<HandleCount; i++) {
-//TODO        CHAR16 *s;
-//TODO        Print(L"i: %d\n", i);
-//TODO        s = GetProtocolInformationDump(HandleBuffer[i], &gEfiBlockIoProtocolGuid, TRUE);
-//TODO        Print (L"Handle %p: %s\n", HandleBuffer[i], s);
-//TODO        SafeFreePool((void**)&s);
-//TODO      }
-//TODO      SafeFreePool((void**)&HandleBuffer);
-//TODO    }
+    else if (StriCmp(argv[1], L"REMOVEALL") == 0) {
+      //remove all non existed ssds from OS
+    }
     else {
       Usage();
       return 0;
--- Apps/ZapNvme.h
+++ Apps/ZapNvme.h
@@ -29,21 +29,13 @@
 #include <Protocol/IdeControllerInit.h>
 #include <Protocol/NvmExpressPassthru.h>
 
-#define nGptSector0 34
-#define nGptSector1 33
-
+// Accourding to NVM Express Documentation
 #define NVME_PRODUCT_MODEL_NUMBER_SIZE 40
 #define NVME_PRODUCT_SERIAL_NUMBER_SIZE 20
 #define NVME_DESCRIPTION_SIZE (NVME_PRODUCT_MODEL_NUMBER_SIZE+NVME_PRODUCT_SERIAL_NUMBER_SIZE+2)
 
-#define USER_NVME_PRODUCT_SERIAL_NUMBER_SIZE 6
-
 
 extern UINT8 Verbose_Level;
-extern UINT8 Dangerous_Zap;
-extern UINT8 Dangerous_Zap_Dry_Run;
-extern UINT8 Verbose_Dump;
-extern UINT8 Save_GPTs;
 
 // UefiBootManagerLib | MdeModulePkg/Library/UefiBootManagerLib/BmBootDescription.c
 CHAR16 *
@@ -56,8 +48,6 @@ CHAR16 *
 BmGetBootDescription (
   IN EFI_HANDLE Handle
 );
-
-VOID hexdump(VOID* data, UINT32 len);
 
 VOID SafeFreePool(void **h);
 VOID SafeShellCloseFile(SHELL_FILE_HANDLE *sfh);
@@ -90,21 +80,7 @@ StrinCmp (
 
 CHAR16 *StrStrip(CHAR16 *src);
 
-
-EFI_STATUS
-SaveGpts (
-  IN EFI_BLOCK_IO_PROTOCOL *BlkIo,
-  IN CHAR16 *Sn
-);
-
-EFI_STATUS
-ZapGpts(
-  IN EFI_BLOCK_IO_PROTOCOL *BlkIo
-);
-
-EFI_STATUS BlockIoProtocolHandles(EFI_HANDLE **HandleBuffer, UINTN *HandleCount);
 INT64 NvmeInfo(VOID);
-EFI_STATUS NvmeZap (CHAR16 *USER_SN, UINTN Length);
 
 #define BlkIo_Terminate -1
 #define BlkIo_Continue -2
@@ -112,5 +88,3 @@ EFI_STATUS NvmeZap (CHAR16 *USER_SN, UINTN Length);
 INT64 BlkIo_Iterator(INT64 index, EFI_HANDLE *Handle, EFI_BLOCK_IO_PROTOCOL **BlkIo);
 INT64 NVME_Iterator(INT64 index, EFI_BLOCK_IO_PROTOCOL **BlkIo, CHAR16 *Desc_Buffer, INT16 Desc_Max);
 CHAR16 *DescToMnSn(CHAR16 *Desc, CHAR16 **Sn);
-EFI_STATUS NvmeGptSave (CHAR16 *USER_SN, UINTN Length);
-EFI_STATUS NvmeDump (CHAR16 *USER_SN, UINTN Length, EFI_LBA Start_Lba, UINT64 Lba_Count);
